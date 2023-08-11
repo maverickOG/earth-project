c1375cc21fc16a307fe469dfb4b155181a06043f
Sajit Magesh <141632950+maverickOG@users.noreply.github.com>
2023-08-11 12:50:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const $climate = document.getElementById("climate");
+const $consumption = document.getElementById("consumption");
+const $resources = document.getElementById("resources");
+const $people = document.getElementById("people");
+
+$climate.onclick =  () =>
+{
+    document.body.style.background = "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/climate-change-bg%5B1%5D.png')"
+    document.body.style.backgroundSize = "100% 100%"
+    $climate.style.backgroundColor = "#2F80ED"
+    $consumption.style.color = "transparent"
+    $resources.style.backgroundColor = "transparent"
+    $people.style.backgroundColor = "transparent"
+    document.getElementById("save").innerHTML = "do your bit! save trees, use renewable energy sources and prefer to travel green."
+    document.getElementById("frame").src = "https://www.theworldcounts.com/embed/challenges/23?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
+};
+
+$consumption.onclick =  () =>
+{
+    document.body.style.background = "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/food-wastage-bg%5B1%5D.png')";
+    document.body.style.backgroundSize = "100% 100%";
+    $consumption.style.backgroundColor = "#27AE60";
+    $slimate.style.color="transparent";
+    $resources.style.backgroundColor = "transparent";
+    $people.style.backgroundColor = "transparent";
+    document.getElementById("save").innerHTML = "do your bit! shop only what you need,eat only what you need and always save the left overs.";
+    document.getElementById("frame").src = "https://www.theworldcounts.com/embed/challenges/107?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20";
+};
+
+$resources.onclick =  () =>
+{
+    document.body.style.background = "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/natural-resources-bg%5B1%5D.png')";
+    document.body.style.backgroundSize = "100% 100%";
+    $resources.style.backgroundColor = "#F2994A";
+    $consumption.style.color="transparent";
+    $climate.style.backgroundColor = "transparent";
+    $people.style.backgroundColor = "transparent";
+    document.getElementById("save").innerHTML = "do your bit! avoid all things plastic. believe in reduce, reuse and recycle";
+    document.getElementById("frame").src = "https://www.theworldcounts.com/embed/challenges/5?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20";
+};
+
+$people.onclick =  () =>
+{
+    document.body.style.background = "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/poverty-bg%5B1%5D.png')";
+    document.body.style.backgroundSize = "100% 100%";
+    $people.style.backgroundColor = "#EB5757";
+    $consumption.style.color="transparent";
+    $resources.style.backgroundColor = "transparent";
+    $climate.style.backgroundColor = "transparent";
+    document.getElementById("save").innerHTML = "do your bit! never waste food. rather offer it to people or animals who are in need.";
+    document.getElementById("frame").src = "https://www.theworldcounts.com/embed/challenges/2?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20";
+};
